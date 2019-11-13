d6ebe996f8e9c14402a548ee8b4e9e9ec560deb9
Marek Pikuła <marek.pikula@sent.tech>
2019-11-13 11:07:34 +0100

    Remove components with declarations in other places
    Quartus doesn't like, when there are multiple declarations of the same module.

--- src/deprecated/cluster_clk_cells_xilinx.sv
+++ src/deprecated/cluster_clk_cells_xilinx.sv
@@ -48,22 +48,6 @@ module cluster_clock_inverter (
 
 endmodule
 
-module pulp_clock_mux2 (
-  input  logic clk0_i,
-  input  logic clk1_i,
-  input  logic clk_sel_i,
-  output logic clk_o
-);
-
-  BUFGMUX i_BUFGMUX (
-    .S  ( clk_sel_i ),
-    .I0 ( clk0_i    ),
-    .I1 ( clk1_i    ),
-    .O  ( clk_o     )
-  );
-
-endmodule
-
 module cluster_clock_xor2 (
   input  logic clk0_i,
   input  logic clk1_i,
